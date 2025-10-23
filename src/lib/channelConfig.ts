879513cae8d3595fc9c31f2a3361585778054e1c
mahdi.107231 <mahdi.107231@gmail.com>
2025-10-23 16:04:38 +0330

    fix channl config

--- src/lib/channelConfig.ts
+++ src/lib/channelConfig.ts
@@ -1,6 +1,15 @@
 import { getChannelConfigMap } from "./channelsService";
+import { LanguageCodeEnum } from "@/gql/graphql";
 
 export async function getChannelConfig(slug: string) {
 	const map = await getChannelConfigMap();
-	return map[slug] ?? map["default-channel"];
+
+	// اگر map خالی بود یا کانال خواسته‌شده پیدا نشد، fallback بده
+	const fallback = {
+		locale: "fa",
+		languageCode: LanguageCodeEnum.FaIr,
+		dir: "rtl" as const,
+	};
+
+	return map?.[slug] ?? map?.["default-channel"] ?? fallback;
 }
